13900ce9fb672ded573b15f311b4a29ee15efe4b
roffatech <roffatech@proton.me>
2025-11-03 20:45:31 -0500

    Add files via upload

---
+++ IS_TOO_NUMERIC.sql
@@ -0,0 +1,40 @@
+DROP FUNCTION IF EXISTS IS_TOO_NUMERIC
+GO
+
+CREATE FUNCTION IS_TOO_NUMERIC
+(
+	@TESTSTR varchar(255),
+	@THRESHOLD decimal(15,4)
+)
+
+RETURNS bit
+AS
+BEGIN
+	DECLARE
+		@STRLEN int,
+		@NUMLEN int,
+		@BITRES bit,
+		@RATIO decimal(15,4)
+
+	SELECT @STRLEN = LEN(@TESTSTR)
+
+	-- We need to know the number of digits in TESTSTR
+	-- The next line of code compares TESTSTR to a string that is 
+	-- the result of converting all the digits to a hashtag character '#'.
+	--
+	-- Example:
+	-- The TRANSLATE function with a TESTSTR of '1A2345ER' becomes '#A####ER'
+	-- the REPLACE function with '#A####ER' becomes 'AER'
+	-- NUMLEN then becomes LEN('1A2345ER') - LEN('AER') = 5
+	SELECT @NUMLEN = LEN(@TESTSTR) - LEN(REPLACE(TRANSLATE(@TESTSTR, '0123456789', '##########'), '#', '')) 
+
+	
+	SELECT @RATIO = CONVERT(decimal(15,4), @NUMLEN) / CONVERT(decimal(15,4), @STRLEN)
+
+	IF @RATIO > @THRESHOLD 
+		SET @BITRES = 1
+	ELSE
+		SET @BITRES = 0
+
+	RETURN @BITRES
+END 
