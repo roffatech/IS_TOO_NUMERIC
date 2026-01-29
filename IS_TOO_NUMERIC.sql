45cff7b99403acaa9d0b34efbaeff10e57038040
roffatech <roffatech@proton.me>
2026-01-28 20:32:00 -0500

    Update IS_TOO_NUMERIC.sql
    Need to handle zero length strings

--- IS_TOO_NUMERIC.sql
+++ IS_TOO_NUMERIC.sql
@@ -18,23 +18,30 @@ BEGIN
 
 	SELECT @STRLEN = LEN(@TESTSTR)
 
-	-- We need to know the number of digits in TESTSTR
-	-- The next line of code compares TESTSTR to a string that is 
-	-- the result of converting all the digits to a hashtag character '#'.
-	--
-	-- Example:
-	-- The TRANSLATE function with a TESTSTR of '1A2345ER' becomes '#A####ER'
-	-- the REPLACE function with '#A####ER' becomes 'AER'
-	-- NUMLEN then becomes LEN('1A2345ER') - LEN('AER') = 5
-	SELECT @NUMLEN = LEN(@TESTSTR) - LEN(REPLACE(TRANSLATE(@TESTSTR, '0123456789', '##########'), '#', '')) 
+	IF @STRLEN = 0 
+		-- If we have a zero-length string passed in, we get division by zero
+		-- so this condition avoids that
+		SET @BITRES = 0
+	ELSE 
+		BEGIN
+			-- We need to know the number of digits in TESTSTR
+			-- The next line of code compares TESTSTR to a string that is 
+			-- the result of converting all the digits to a hashtag character '#'.
+			--
+			-- Example:
+			-- The TRANSLATE function with a TESTSTR of '1A2345ER' becomes '#A####ER'
+			-- the REPLACE function with '#A####ER' becomes 'AER'
+			-- NUMLEN then becomes LEN('1A2345ER') - LEN('AER') = 5
+			SELECT @NUMLEN = LEN(@TESTSTR) - LEN(REPLACE(TRANSLATE(@TESTSTR, '0123456789', '##########'), '#', '')) 
 
 	
-	SELECT @RATIO = CONVERT(decimal(15,4), @NUMLEN) / CONVERT(decimal(15,4), @STRLEN)
+			SELECT @RATIO = CONVERT(decimal(15,4), @NUMLEN) / CONVERT(decimal(15,4), @STRLEN)
 
-	IF @RATIO > @THRESHOLD 
-		SET @BITRES = 1
-	ELSE
-		SET @BITRES = 0
+			IF @RATIO > @THRESHOLD 
+				SET @BITRES = 1
+			ELSE
+				SET @BITRES = 0
+		END
 
 	RETURN @BITRES
 END 
